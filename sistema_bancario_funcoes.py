2e3419bb0872ffd7984f0012abd671e16bfadc15
carlos4565mana <carloscal61@gmail.com>
2025-08-29 21:05:41 -0300

    implementando as funcoes de criar novo usuario e filtar usuario

--- sistema_bancario_funcoes.py
+++ sistema_bancario_funcoes.py
@@ -18,8 +18,48 @@ def menu():
 
 def main():
   """Função principal do sistema"""
+  saldo = 0
+  numeros_saques = 0
+  extrato = ""
+  usuarios = []
+  contas = []
+
+
   while True:
     opcao = menu()
+    match opcao:
+      case "1":
+        valor = float(input("Digite o valor do deposito: "))
+        saldo,extrato = deposito(saldo,valor,extrato)
+      case "4":
+        usuarios = criar_usuario(usuarios)
+
+      case "0":
+        print("\nObrigado por utilizar o nossos serviços volte sempre!!")
+        break
+      case _:
+        print("⚠️ Operação inválida! Selecione novamente.")
+
+def criar_usuario(usuarios):
+   """Cadastra um novo usuário no sistema"""
+   cpf = input("Informe o CPF (somente número): ")
+   usuario = filtrar_usuario(cpf, usuarios)
+   if usuario:
+    print("\n⚠️ Já existe usuário com esse CPF!")
+    return
+   nome = input("Digite nome completo: ")
+   data_nascimento = input("Digite a data de nascimento no formato dia/mes/ano: ")
+   endereco = input("Digite o endereço completo (logradouro, nº, bairro, cidade/sigla do estado): ")
+   usuarios.append({ "nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+   print("✅ Usuário criado com sucesso!")
+   return usuarios
+
+
+def filtrar_usuario(cpf, usuarios):
+  usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+  return usuarios_filtrados[0] if usuarios_filtrados else None
+
+    
 
 if __name__ == "__main__":
   main()
